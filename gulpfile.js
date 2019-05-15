bec5bf704d4d6e8741ae71e2ee4b7ad299ee49e2
Mike Stevenson <michaelhstevenson@gmail.com>
2019-05-14 22:55:54 -0700

    sdf

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,60 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var touch = require('gulp-touch-cmd');
+var gulp = require('gulp'),
+    shell = require('gulp-shell'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    touch = require('gulp-touch-cmd'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    uncss = require('postcss-uncss'),
+    pfm = require('postcss-font-magician'),
+    utilities = require('postcss-utilities'),
+    stylelint = require('gulp-stylelint'),
+    reporter    = require('postcss-reporter'),
+    csso = require('postcss-csso');
 
-/*
-  Build and watch Jekyll (change this task to whatever you need)
-*/
-gulp.task('generate', shell.task('bundle exec jekyll serve --watch --livereload -o'));
+gulp.task('generate', shell.task('bundle exec jekyll serve --watch --livereload'));
 gulp.task('buildit', shell.task('bundle exec jekyll build -d _site'));
-gulp.task('preview_jekyll', shell.task('bundle exec jekyll build --drafts --unpublished --future -d _site'));
-gulp.task('buildme', shell.task('bundle exec jekyll build --watch'));
-/*
-  Compile SCSS files to CSS
-*/
 
 gulp.task('styles', function () {
+
+    var processors = [
+        utilities(),
+        autoprefixer({
+            "browsers": [
+              "> 1%",
+              "last 2 versions",
+              "IE 9"
+            ]
+          }),
+        csso({comments: false}),
+      ];
+
     return gulp.src('./assets/css/main.scss')
     .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
+        // .pipe(sass({
+        //     outputStyle: 'compressed'
+        // }).on('error', sass.logError))
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(postcss(processors))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('css/'))
         .pipe(touch());
 });
 
-/*
-  Compile the assets
-*/
-// gulp.task('assets', gulp.parallel(
-//     'styles'
-// ));
-
-gulp.task('build', gulp.series(
-    'styles',
-    'generate'
-));
-
+// Deploy task on forestry.io
 gulp.task('deploy', gulp.series(
     'styles',
     'buildit'
 ));
 
+// Watch scss changes
 gulp.task('watch', function() {
     gulp.watch('assets/**/*.scss', gulp.series('styles'));
   });
 
-// Watch and build
-  gulp.task('default', gulp.parallel(
+// Default: watch and build local
+gulp.task('default', gulp.parallel(
     'watch',
     'generate'
-));
-
-// Preview
-gulp.task('preview', gulp.series(
-    'styles',
-    'preview_jekyll'
 ));
