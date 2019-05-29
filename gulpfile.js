f1e85f2bcb9fdab6d348e34cd8daa66e44332e26
Mike Stevenson <michaelhstevenson@gmail.com>
2019-05-28 20:14:23 -0700

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -21,13 +21,14 @@ gulp.task('scss-local', function () {
         replace: false
     };
     var postcssOptions = {
-        map: false
+        map: true,
+        "map.inline": false
     };
     var processors = [
-        // uncss({
-        //     html: ['./_site/**/*.html'],
-        //     ignore: ['.fade']
-        // }),
+        uncss({
+            html: ['./_site/**/*.html'],
+            ignore: ['.fade']
+        }),
         utilities(),
         autoprefixer({
             "browsers": [
@@ -39,28 +40,18 @@ gulp.task('scss-local', function () {
         csso({
             comments: false
         })
-    ],
-    processorsUnminify = [
-        utilities(),
-        autoprefixer({
-            "browsers": [
-                "> 1%",
-                "last 2 versions",
-                "IE 9"
-            ]
-        })
     ];
-return gulp.src('./assets/css/main.scss')
-.pipe(sourcemaps.init({
-    loadMaps: true
-}))
-.pipe(sass())
-.on("error", sass.logError)
-.pipe(pxtorem(pxtoremOptions, postcssOptions))
-.pipe(postcss(processors))
-.pipe(sourcemaps.write('.'))
-.pipe(gulp.dest('css/'))
-.pipe(touch());
+    return gulp.src('./assets/css/main.scss')
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(postcss(processors))
+        .pipe(pxtorem(pxtoremOptions))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('css/'))
+        .pipe(touch());
 });
 
 
