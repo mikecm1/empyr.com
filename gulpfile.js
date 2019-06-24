6d43622ae10ffe54683eebd85e494c2b9f1988ba
Mike Stevenson <mike@mike.cm>
2019-06-24 13:14:38 -0700

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var gulp = require('gulp'),
     replace = require('gulp-replace'),
     csso = require('postcss-csso');
 
-gulp.task('generate', shell.task('bundle exec jekyll serve --watch --incremental --livereload'));
+gulp.task('generate', shell.task('bundle exec jekyll serve --watch --livereload'));
 gulp.task('buildit', shell.task('bundle exec jekyll build -d _site'));
 
 gulp.task('scss-local', function () {
