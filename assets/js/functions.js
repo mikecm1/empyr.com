e73858d22d1d24415cde64cb0e3a3b0d69f8de82
Mike Stevenson <mike@mike.cm>
2019-07-11 15:38:56 -0700

    ---

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     //     var Scrollbar = window.Scrollbar;
     // $('body').addClass('smooth');
     //     const options = {
@@ -11,104 +11,137 @@ $( document ).ready(function() {
     //     };
     //     Scrollbar.init(document.querySelector("html:not(.no-js) body.smooth #main-scrollbar"), options);
 
-        $('[data-id=headers-4-sidebar-toggle]').click(function (e) {
-            e.preventDefault();
-            $('#headers-4-sidebar').toggleClass('show');
-        });
-        $('[data-id=headers-4-sidebar-close]').click(function (e) {
-            e.preventDefault();
-            $('#headers-4-sidebar').removeClass('show');
-        });
+    $('[data-id=headers-4-sidebar-toggle]').click(function (e) {
+        e.preventDefault();
+        $('#headers-4-sidebar').toggleClass('show');
+    });
+    $('[data-id=headers-4-sidebar-close]').click(function (e) {
+        e.preventDefault();
+        $('#headers-4-sidebar').removeClass('show');
+    });
 
-        var transEffect = Barba.BaseTransition.extend({
-            start: function(){
-              this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
-            },
-            fadeInNewcontent: function(nc) {
-              nc.hide();
-              var _this = this;
-              $(this.oldContainer).fadeOut(300).promise().done(() => {
-                nc.css('visibility','visible');
-                nc.fadeIn(300, function(){
-                  _this.done();
+    var transEffect = Barba.BaseTransition.extend({
+        start: function () {
+            this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
+        },
+        fadeInNewcontent: function (nc) {
+            nc.hide();
+            var _this = this;
+            $(this.oldContainer).fadeOut(300).promise().done(() => {
+                // $(function () {
+                //     var pathName = document.location.pathname;
+                //     window.onbeforeunload = function () {
+                //         var scrollPosition = $(document).scrollTop();
+                //         sessionStorage.setItem("scrollPosition_" + pathName, scrollPosition.toString());
+                //     }
+                //     if (sessionStorage["scrollPosition_" + pathName]) {
+                //         $(document).scrollTop(sessionStorage.getItem("scrollPosition_" + pathName));
+                //     }
+                // });
+                nc.css('visibility', 'visible');
+                document.body.scrollTop = 0;
+                nc.fadeIn(300, function () {
+                    _this.done();
                 })
-              });
+            });
+        }
+    });
+    Barba.Pjax.getTransition = function () {
+        return transEffect;
+    }
+    Barba.Pjax.start();
+    Barba.Prefetch.init();
+    Barba.Dispatcher.on('transitionCompleted', function (currentStatus, oldStatus, container) {
+        var swiper = new Swiper('.swiper-container', {
+            autoplay: {
+                delay: 5000,
+            },
+            loop: true,
+            effect: 'crossfade',
+            spaceBetween: 0,
+            slidesOffsetAfter: 100,
+            centeredSlides: true,
+            slideToClickedSlide: true,
+            loopedSlides: 0,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            slidesPerView: 3,
+            spaceBetween: 0,
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                480: {
+                    slidesPerView: 2,
+                },
             }
         });
-        Barba.Pjax.getTransition = function() {
-          return transEffect;
+
+    });
+    Barba.Dispatcher.on('newPageReady', function (currentStatus, oldStatus, container, newPageRawHTML) {
+        var $newPageHead = $('<head />').html(
+            $.parseHTML(
+                newPageRawHTML.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0], document, true
+            )
+        );
+        var headTags = [
+            "meta[name='keywords']", "meta[name='description']", "meta[property^='og']", "meta[name^='twitter']", "meta[itemprop]", "link[itemprop]", "link[rel='prev']", "link[rel='next']", "link[rel='canonical']"
+        ].join(',');
+        $('head').find(headTags).remove();
+        $newPageHead.find(headTags).appendTo('head');
+    });
+
+    Barba.Dispatcher.on('newPageReady', function (currentStatus) {
+        const link = currentStatus.url.split(window.location.origin)[1].substring(1);
+        const navigation = document.querySelector('.navbar-nav');
+        const navigationLinks = navigation.querySelectorAll('.nav-link');
+        const navigationLinkIsActive = navigation.querySelector(("a[href='" + link + "']"));
+        Array.prototype.forEach.call(navigationLinks, (navigationLink) => navigationLink.classList.remove('active'));
+        // navigationLinkIsActive.classList.add('active');
+    });
+    Barba.Dispatcher.on('initStateChange', function () {
+        if (Barba.HistoryManager.history.length <= 1) {
+            return;
         }
-        Barba.Pjax.start();
-                Barba.Prefetch.init();
-                Barba.Dispatcher.on('transitionCompleted', function(currentStatus, oldStatus, container) {
-                    var swiper = new Swiper('.swiper-container', {
-                        autoplay: {
-                            delay: 5000,
-                        },
-                        loop: true,
-                        effect: 'crossfade',
-                        spaceBetween: 0,
-                        slidesOffsetAfter: 100,
-                        centeredSlides: true,
-                        slideToClickedSlide: true,
-                        loopedSlides: 0,
-                        pagination: {
-                            el: '.swiper-pagination',
-                            clickable: true,
-                        },
-                        slidesPerView: 3,
-                        spaceBetween: 0,
-                        breakpoints: {
-                          320: {
-                            slidesPerView: 1,
-                          },
-                          480: {
-                            slidesPerView: 2,
-                          },
-                        }
-                    });
-
-                  });
-                  Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container, newPageRawHTML) {
-                    // html head parser borrowed from jquery pjax
-                    var $newPageHead = $( '<head />' ).html(
-                        $.parseHTML(
-                            newPageRawHTML.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0]
-                          , document
-                          , true
-                        )
-                    );
-                    var headTags = [
-                        "meta[name='keywords']"
-                      , "meta[name='description']"
-                      , "meta[property^='og']"
-                      , "meta[name^='twitter']"
-                      , "meta[itemprop]"
-                      , "link[itemprop]"
-                      , "link[rel='prev']"
-                      , "link[rel='next']"
-                      , "link[rel='canonical']"
-                    ].join(',');
-                    $( 'head' ).find( headTags ).remove(); // Remove current head tags
-                    $newPageHead.find( headTags ).appendTo( 'head' ); // Append new tags to the head
+        if (typeof gtag === 'function') {
+            gtag('config', 'UA-77237997-1', {'page_path': location.pathname, 'use_amp_client_id': true});
+        } else {
+            if (typeof ga === 'function') {
+                var trackers = ga.getAll();
+                trackers.forEach(function (tracker) {
+                    ga(tracker.get('name') + '.send', 'pageview', location.pathname, {'useAmpClientId': true});
                 });
-
-        Barba.Dispatcher.on('newPageReady', function (currentStatus) {
-            const link = currentStatus.url.split(window.location.origin)[1].substring(1); // get path of current page
-            const navigation             = document.querySelector('.navbar-nav');
-            const navigationLinks        = navigation.querySelectorAll('.nav-link');
-            const navigationLinkIsActive = navigation.querySelector(("a[href='" + link + "']"));
-            Array.prototype.forEach.call(navigationLinks, (navigationLink) => navigationLink.classList.remove('active')); // remove CSS class 'is-active' from all .navigation__links
-            // navigationLinkIsActive.classList.add('active');
-        });
-
-        document.documentElement.className = 'js';
-        AOS.init({
-            once: !0,
-            duration: 400,
-            disable: 'mobile',
-            anchorPlacement: 'top'
-        });
+            }
+        }
+    });
+    Barba.Pjax.originalPreventCheck = Barba.Pjax.preventCheck;
+    Barba.Pjax.preventCheck = function(evt, element) {
+        if (!Barba.Pjax.originalPreventCheck(evt, element)) {
+            return false;
+        }
+        if (/.pdf/.test(element.href.toLowerCase())) {
+            return false;
+        }
+        var ignoreClasses = [
+            '.pdf', '.jpg', '.jpeg', '.png', '.gif', '.mp4', '.avi',
+            '.mp3', '.csv', '.txt', '.flv', '.wmv', '.tar', '.zip', '.gzip', '.3gp'
+        ];
+        for (var i = 0; i < ignoreClasses.length; i++) {
+            if (element.classList.contains(ignoreClasses[i])) {
+                return false;
+            }
+        }
+        return true;
+    };
+    document.documentElement.className = 'js';
+    AOS.init({
+        once: !0,
+        duration: 400,
+        disable: 'mobile',
+        anchorPlacement: 'top'
+    });
 
     var swiper = new Swiper('.swiper-container', {
         autoplay: {
@@ -128,15 +161,17 @@ $( document ).ready(function() {
         slidesPerView: 3,
         spaceBetween: 0,
         breakpoints: {
-          320: {
-            slidesPerView: 1,
-          },
-          480: {
-            slidesPerView: 2,
-          },
+            320: {
+                slidesPerView: 1,
+            },
+            480: {
+                slidesPerView: 2,
+            },
         }
     });
-    $(".navbar a").on('click',function(){
-        history.pushState({scrollTop:document.body.scrollTop},document.title,document.location.pathname);
+    $(".navbar a").on('click', function () {
+        history.pushState({
+            scrollTop: document.body.scrollTop
+        }, document.title, document.location.pathname);
     });
 });
